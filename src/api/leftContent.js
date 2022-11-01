f5c14ba2520b47fa7579b1ddc094faee30d15ef8
Invoker~ <janvincent.claudio@gmail.com~>
2022-11-01 18:46:58 +0800

    Update

---
+++ src/api/leftContent.js
@@ -0,0 +1,9 @@
+export const loginBtn = [
+  {
+    userOut: "Login",
+  },
+  {
+    userOut: "Register",
+    userIn: ["Wallet", "Withdraw", "Deposit"]
+  }
+]
--- src/components/leftContent/userInfo/userInfo.js
+++ src/components/leftContent/userInfo/userInfo.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react'
 import classNames from 'classnames';
+import { loginBtn } from '../../../api/leftContent';
 // Styles
 import styles from './userInfo.module.scss'; 
 
 const UserInfo = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
    <>
     <div className={styles.userInfoContainer}>
       <div className={styles.avatarContainer}>
         <div className={styles.avatar}></div>
         <div className={styles.btnGrp}>
-          <div>Login</div>
-          <div>Register</div>
+          {loginBtn?.map((item, i) => (
+            <div>{isLogin ? item.userIn : item.userOut}</div>
+          ))}
+          
         </div>
       </div>
     </div>
