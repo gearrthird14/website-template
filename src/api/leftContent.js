7422f60a77fa49122624f596ef8b92fe9a1c6987
gearrthird14 <gearthird14@gmail.com>
2022-11-02 01:45:28 +0800

    update

--- src/api/leftContent.js
+++ src/api/leftContent.js
@@ -1,9 +1,46 @@
 export const loginBtn = [
   {
     userOut: "Login",
+    userIn: "Wallet", 
   },
   {
     userOut: "Register",
-    userIn: ["Wallet", "Withdraw", "Deposit"]
+    userIn: "Withdraw"
+  },
+  {
+    userIn: "Deposit"
   }
+]
+
+export const sideVenue = [
+  {
+    sideVenue: "Sponsor"
+  },
+  {
+    sideVenue: "Agent"
+  },
+  {
+    sideVenue: "WEWE"
+  },
+  {
+    sideVenue: "ASD"
+  },
+  {
+    sideVenue: "ZXc"
+  },
+  {
+    sideVenue: "APP"
+  },
+]
+
+export const appDownload = [
+  {
+    appTabs: "PC下载"
+  },
+  {
+    appTabs: "Sport下载"
+  },
+  {
+    appTabs: "iOS下载"
+  },
 ]
--- src/components/leftContent/userInfo/userInfo.js
+++ src/components/leftContent/userInfo/userInfo.js
@@ -1,22 +1,68 @@
 import React, { useState } from 'react'
 import classNames from 'classnames';
 import { loginBtn } from '../../../api/leftContent';
+import { sideVenue } from '../../../api/leftContent';
+import { appDownload } from '../../../api/leftContent';
 // Styles
 import styles from './userInfo.module.scss'; 
 
 const UserInfo = () => {
+
   const [isLogin, setIsLogin] = useState(false);
+  const [hasPCDownload, setHasPCDownload] = useState(false);
+  const [sportsAppDownload, setSportsAppDownload] = useState(false)
 
   return (
    <>
-    <div className={styles.userInfoContainer}>
+    <div className={styles.leftContent}>
       <div className={styles.avatarContainer}>
         <div className={styles.avatar}></div>
         <div className={styles.btnGrp}>
-          {loginBtn?.map((item, i) => (
-            <div>{isLogin ? item.userIn : item.userOut}</div>
+          {loginBtn?.map((item) => (
+            <>
+              { isLogin ? ( 
+                <div className={styles.btnTab1}>
+                  {item.userIn}
+                </div>
+              ) : (
+              <div className={classNames ({
+                [styles.btnTab2]:true,
+                [styles.btnTab3]: !isLogin
+              })}>
+                  {item.userOut}
+                </div>
+              )}
+            </>
           ))}
-          
+        </div>
+      </div>
+      <div className={styles.sideVenueContainer}>
+        <div className={styles.sideVenueList}>
+          {sideVenue?.map((item, i) => (
+            <div 
+            key={i}
+            className={styles.sideVenueTabs}>
+              {item.sideVenue}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.appDownloadContainer}>
+        <div className={styles.appDownloadImage}>
+          IMAGE
+        </div>
+        <div className={classNames ({
+          [styles.appDownloadList]:true,
+        })}>
+            {appDownload?.map((item) => (
+              <div className={classNames ({
+                [styles.appDownloadTabs]: true,
+                [styles.hasPCDownload2]: hasPCDownload || (hasPCDownload  && !sportsAppDownload) ,
+                [styles.sportsAppDownload2]: sportsAppDownload 
+              })}>
+                {item?.appTabs}
+              </div>
+            ))}
         </div>
       </div>
     </div>
